e1efee1c4079850fb91aa3f1abf5c8cc7d58aff6
i <i@ohyooo.com>
2023-12-27 22:11:31 +0800

    fix desktop link

--- shared/src/desktopMain/kotlin/com/ohyooo/shared/platform.desktop.kt
+++ shared/src/desktopMain/kotlin/com/ohyooo/shared/platform.desktop.kt
@@ -7,14 +7,13 @@ import java.util.Locale
 actual fun getPlatformName() = "Desktop"
 
 actual fun openGitHub() {
-    val uri = URI("https://github.com/ohyooo/JPSyllabary-KMM")
     val osName by lazy(LazyThreadSafetyMode.NONE) { System.getProperty("os.name").lowercase(Locale.getDefault()) }
     val desktop = Desktop.getDesktop()
     when {
-        Desktop.isDesktopSupported() && desktop.isSupported(Desktop.Action.BROWSE) -> desktop.browse(uri)
-        "mac" in osName -> Runtime.getRuntime().exec("open $uri")
-        "nix" in osName || "nux" in osName -> Runtime.getRuntime().exec("xdg-open $uri")
-        else -> throw RuntimeException("cannot open $uri")
+        Desktop.isDesktopSupported() && desktop.isSupported(Desktop.Action.BROWSE) -> desktop.browse(URI(GITHUB_LINK))
+        "mac" in osName -> Runtime.getRuntime().exec(arrayOf("open", GITHUB_LINK), null, null)
+        "nix" in osName || "nux" in osName -> Runtime.getRuntime().exec(arrayOf("xdg-open", GITHUB_LINK), null, null)
+        else -> throw RuntimeException("cannot open $GITHUB_LINK")
     }
 }
 
