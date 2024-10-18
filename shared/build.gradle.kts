63a9610da1eafac25eb3a7254a8951edd9f250fb
i <i@ohyooo.com>
2024-10-18 15:42:35 +0800

    update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+@file:OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
@@ -18,7 +16,6 @@ version = "1.0.0"
 kotlin {
     androidTarget()
     jvm("desktop")
-    @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "shared"
         browser {
